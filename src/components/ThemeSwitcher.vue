192c8f917d2a67fa880d7a8d1b0b447bcf7caf7f
Melinda Golden <info@melindagolden.com>
2022-03-20 12:41:43 -0400

    add: add theme switcher to default layout

---
+++ src/components/ThemeSwitcher.vue
@@ -0,0 +1,59 @@
+<template>
+  <button
+    :aria-label="theme === 'light' ? 'Toggle dark theme' : 'Toggle light theme'"
+    @click="toggleTheme"
+  >
+    <Sun v-if="theme === 'light'" class="w-6 ml-4" />
+    <Moon v-else class="w-6 ml-4" />
+  </button>
+</template>
+
+<script>
+import Sun from "~/assets/svgs/Sun.svg";
+import Moon from "~/assets/svgs/Moon.svg";
+
+export default {
+  components: {
+    Sun,
+    Moon
+  },
+
+  data() {
+    return {
+      theme: ""
+    };
+  },
+
+  mounted() {
+    const isDarkTheme =
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+    const defaultTheme = isDarkTheme ? "dark" : "light";
+
+    const theme = localStorage.getItem("THEME") || defaultTheme;
+
+    this.setTheme(theme);
+  },
+
+  methods: {
+    toggleTheme() {
+      const newTheme = this.theme === "light" ? "dark" : "light";
+
+      this.setTheme(newTheme);
+    },
+
+    setTheme(newTheme) {
+      this.theme = newTheme;
+
+      if (this.theme === "dark") {
+        document.documentElement.classList.add("dark");
+      } else {
+        document.documentElement.classList.remove("dark");
+      }
+
+      localStorage.setItem("THEME", newTheme);
+    }
+  }
+};
+</script>
