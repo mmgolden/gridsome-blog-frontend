9509a0353f20d3d75171d2a499287eb413ab3fb9
Melinda Golden <info@melindagolden.com>
2022-03-22 20:04:34 -0400

    add: built with blurb in footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,9 +2,16 @@
   <footer class="px-4 2xl:px-0 py-7 mt-auto">
     <div class="container mx-auto lg:flex items-center justify-between">
       <!-- Copyright -->
-      <p class="text-base font-medium text-navy dark:text-gray">
-        © Copyright {{ currentYear }} {{ $static.metadata.siteName }}
-      </p>
+      <div class="lg:flex">
+        <p class="text-base font-medium text-navy dark:text-gray">
+          © Copyright {{ currentYear }} {{ $static.metadata.siteName }}
+        </p>
+        <p class="hidden lg:block mx-2">|</p>
+        <p class="pt-2 lg:pt-0 text-base font-medium text-navy dark:text-gray">
+          Built with <a href="https://strapi.io/" target="_blank">Strapi</a> and
+          <a href="https://gridsome.org/" target="_blank">Gridsome</a>
+        </p>
+      </div>
 
       <!-- Links -->
       <p class="pt-2 lg:pt-0 text-base font-medium text-navy dark:text-gray">
