672c432fdf9050ae551ebcdc464b2101fe11ba49
Melinda Golden <info@melindagolden.com>
2022-03-24 22:11:52 -0400

    add: article template

---
+++ src/templates/Article.vue
@@ -0,0 +1,73 @@
+<template>
+  <Layout>
+    <PageTitle :title="article.title" />
+  </Layout>
+</template>
+
+<page-query>
+query GetArticle($id: ID!) {
+  strapi {
+    article(id: $id) {
+      data {
+        id
+        attributes {
+          title
+          description
+          slug
+          author {
+            data {
+              id
+              attributes {
+                name
+                avatar_url
+              }
+            }
+          }
+          category {
+            data {
+              id
+              attributes {
+                name
+                slug
+              }
+            }
+          }
+          body
+          publishedAt
+        }
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+import PageTitle from "@/components/PageTitle.vue";
+
+export default {
+  metaInfo() {
+    return {
+      title: this.article.title,
+      titleTemplate: "%s"
+    };
+  },
+
+  components: {
+    PageTitle
+  },
+
+  computed: {
+    articleId() {
+      return this.$page.strapi.article.data.id;
+    },
+
+    article() {
+      return this.$page.strapi.article.data.attributes;
+    }
+  },
+
+  mounted() {
+    window.console.log(this.$page.strapi);
+  }
+};
+</script>
