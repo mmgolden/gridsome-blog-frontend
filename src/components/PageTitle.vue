c604e51161a438d91ab6b2946d4b7cb9c13dea22
Melinda Golden <info@melindagolden.com>
2022-03-24 22:10:40 -0400

    add: page title component

---
+++ src/components/PageTitle.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="bg-gray-dark dark:bg-navy-dark">
+    <div
+      class="container mx-auto py-28 flex flex-col justify-center items-center"
+    >
+      <h1
+        class="text-4xl lg:text-5xl text-center font-bold text-navy dark:text-gray"
+      >
+        {{ title }}
+      </h1>
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
