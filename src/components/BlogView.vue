acc24fc47b284ddbd66d6c13a43d97e70c9bcf64
Melinda Golden <info@melindagolden.com>
2022-03-25 21:56:24 -0400

    add: blog view component

---
+++ src/components/BlogView.vue
@@ -0,0 +1,44 @@
+<template>
+  <Layout>
+    <!-- Title -->
+    <PageTitle title="Blog" />
+
+    <!-- Articles -->
+    <div class="container mx-auto pt-44 px-4 2xl:px-0">
+      <div v-for="article in articles" :key="article.id">
+        <ArticleCard :article="article" />
+
+        <div class="w-full h-px my-10 bg-border dark:bg-gray" />
+      </div>
+    </div>
+
+    <!-- Pagination -->
+    <Pagination :pagination="pagination" />
+  </Layout>
+</template>
+
+<script>
+import PageTitle from "@/components/PageTitle.vue";
+import ArticleCard from "@/components/ArticleCard.vue";
+import Pagination from "@/components/Pagination.vue";
+
+export default {
+  components: {
+    PageTitle,
+    ArticleCard,
+    Pagination
+  },
+
+  props: {
+    articles: {
+      type: Array,
+      required: true
+    },
+
+    pagination: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
