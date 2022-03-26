472b177bc2fadb99f61b1f979d25f59a2bf92e23
Melinda Golden <info@melindagolden.com>
2022-03-26 13:01:40 -0400

    add: markdown component

---
+++ src/components/Markdown.vue
@@ -0,0 +1,29 @@
+<template>
+  <VueMarkdown :source="source" />
+</template>
+
+<script>
+import VueMarkdown from "vue-markdown";
+import Prism from "prismjs";
+import "prismjs/themes/prism-okaidia.css";
+import "prismjs/components/prism-javascript.min.js";
+
+export default {
+  components: {
+    VueMarkdown
+  },
+
+  props: {
+    source: {
+      type: String,
+      required: true
+    }
+  },
+
+  mounted() {
+    this.$nextTick(() => {
+      Prism.highlightAll();
+    });
+  }
+};
+</script>
