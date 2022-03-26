4523dd6219c86ac34327496412ea33ad7ed953f6
Melinda Golden <info@melindagolden.com>
2022-03-26 18:26:28 -0400

    add: 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,31 @@
+<template>
+  <Layout>
+    <!-- Title -->
+    <PageTitle title="Page Not Found" />
+
+    <!-- Content -->
+    <div class="container mx-auto py-16 lg:py-32 px-4 2xl:px-0">
+      <p
+        class="text-2xl text-center font-bold leading-normal text-navy dark:text-gray"
+      >
+        Oops, I can't seem to find that page. Want to return
+        <g-link to="/">home</g-link>?
+      </p>
+    </div>
+  </Layout>
+</template>
+
+<script>
+import PageTitle from "@/components/PageTitle.vue";
+
+export default {
+  metaInfo: {
+    title: "Page Not Found",
+    titleTemplate: "%s | Melinda Golden"
+  },
+
+  components: {
+    PageTitle
+  }
+};
+</script>
