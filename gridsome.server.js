5982d277ce5a592ab18cc841e64fc854cc8949d6
Melinda Golden <info@melindagolden.com>
2022-03-25 21:53:58 -0400

    add: generate paginated blog pages

--- gridsome.server.js
+++ gridsome.server.js
@@ -1,4 +1,67 @@
 module.exports = function (api) {
+  // Create paginated blog pages
+  api.createPages(async ({ graphql, createPage }) => {
+    // Get total page count
+    const { data: pageCountData } = await graphql(`
+      {
+        strapi {
+          articles(pagination: { page: 1, pageSize: 6 }) {
+            meta {
+              pagination {
+                total
+                page
+                pageSize
+                pageCount
+              }
+            }
+          }
+        }
+      }
+    `);
+
+    const pageCount = pageCountData.strapi.articles.meta.pagination.pageCount;
+
+    // Start loop at 2 - do not need /blog/1
+    for (let i = 2; i <= pageCount; i++) {
+      // Get pagination data for blog page
+      const { data: paginationData } = await graphql(`
+      {
+        strapi {
+          articles(pagination: { page: ${i}, pageSize: 6 }) {
+            meta {
+              pagination {
+                total
+                page
+                pageSize
+                pageCount
+              }
+            }
+          }
+        }
+      }
+    `);
+
+      const {
+        total,
+        page,
+        pageSize,
+        pageCount
+      } = paginationData.strapi.articles.meta.pagination;
+
+      createPage({
+        path: `/blog/${i}`,
+        component: "./src/templates/BlogTemplate.vue",
+        context: {
+          blogTotal: total,
+          blogPage: page,
+          blogPageSize: pageSize,
+          blogPageCount: pageCount
+        }
+      });
+    }
+  });
+
+  // Create individual article pages
   api.createPages(async ({ graphql, createPage }) => {
     const { data } = await graphql(`
       {
@@ -18,7 +81,7 @@ module.exports = function (api) {
     data.strapi.articles.data.forEach((node) => {
       createPage({
         path: `/blog/${node.attributes.slug}`,
-        component: "./src/templates/Article.vue",
+        component: "./src/templates/ArticleTemplate.vue",
         context: {
           id: node.id
         }
