ef55140ba06b2ceea8edc80a332d0533df8c61ea
Melinda Golden <info@melindagolden.com>
2022-03-26 18:24:41 -0400

    add: highlight to h1 in hero

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -6,7 +6,7 @@
         <h1
           class="text-4xl lg:text-5xl mb-2.5 font-bold text-navy dark:text-gray"
         >
-          Hello, I'm Melinda Golden.
+          Hello, I'm <span class="highlight">Melinda Golden</span>.
         </h1>
         <p class="text-2xl lg:text-3xl font-medium text-navy dark:text-gray">
           I'm a front-end developer based in Asheville, NC.
@@ -16,7 +16,12 @@
       <!-- Image -->
       <div class="relative lg:flex justify-center">
         <div class="image absolute -bottom-24 2xl:-bottom-20 lg:ml-14">
-          <g-image src="~/assets/pngs/cutout.png" quality="100" immediate />
+          <g-image
+            src="~/assets/pngs/cutout.png"
+            quality="100"
+            immediate
+            alt="Headshot of Melinda Golden"
+          />
         </div>
       </div>
     </div>
@@ -27,5 +32,22 @@
 .image {
   width: 100%;
   max-width: 359px;
+}
+
+.highlight {
+  position: relative;
+  display: inline-block;
+  z-index: 9;
+}
+
+.highlight::after {
+  content: "";
+  width: 100%;
+  height: 14px;
+  background: #3abfc8;
+  position: absolute;
+  left: 0;
+  bottom: 5%;
+  z-index: -1;
 }
 </style>
