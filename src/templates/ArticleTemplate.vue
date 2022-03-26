81c7fa235c7080b0cceefa94d9c4f081d8edc27a
Melinda Golden <info@melindagolden.com>
2022-03-26 13:02:09 -0400

    add: markdown to article template

--- src/templates/ArticleTemplate.vue
+++ src/templates/ArticleTemplate.vue
@@ -1,6 +1,24 @@
 <template>
   <Layout>
-    <PageTitle :title="article.title" />
+    <!-- Title -->
+    <PageTitle :title="article.title">
+      <div
+        class="text-lg font-medium pb-5 lg:pb-0 text-navy dark:text-gray mt-4"
+      >
+        {{ formatPublishDate(article.publishedAt) }}
+        -
+        <g-link :to="`/category/${article.category.data.attributes.slug}`">
+          {{ article.category.data.attributes.name }}
+        </g-link>
+      </div>
+    </PageTitle>
+
+    <!-- Content -->
+    <div
+      class="article-content container mx-auto py-44 text-lg font-medium text-navy dark:text-gray px-4 2xl:px-0"
+    >
+      <Markdown :source="article.body" />
+    </div>
   </Layout>
 </template>
 
@@ -43,6 +61,8 @@ query GetArticle($id: ID!) {
 
 <script>
 import PageTitle from "@/components/PageTitle.vue";
+import Markdown from "@/components/Markdown.vue";
+import formatDate from "@/utils/formatDate.js";
 
 export default {
   metaInfo() {
@@ -53,13 +73,98 @@ export default {
   },
 
   components: {
-    PageTitle
+    PageTitle,
+    Markdown
   },
 
   computed: {
     article() {
       return this.$page.strapi.article.data.attributes;
     }
+  },
+
+  methods: {
+    formatPublishDate(date) {
+      return formatDate(date);
+    }
   }
 };
 </script>
+
+<style scoped>
+::v-deep .article-content h1 {
+  font-size: 3rem;
+  font-weight: 700;
+  line-height: 1;
+  margin-bottom: 1.5rem;
+}
+
+::v-deep .article-content h2 {
+  font-size: 2.25rem;
+  font-weight: 700;
+  line-height: 2.5rem;
+  margin-bottom: 1.5rem;
+}
+
+::v-deep .article-content h3 {
+  font-size: 1.875rem;
+  font-weight: 700;
+  line-height: 2.25rem;
+  margin-bottom: 1.5rem;
+}
+
+::v-deep .article-content h4 {
+  font-size: 1.5rem;
+  font-weight: 700;
+  line-height: 2rem;
+  margin-bottom: 1.5rem;
+}
+
+::v-deep .article-content h5 {
+  font-size: 1.25rem;
+  font-weight: 700;
+  line-height: 1.75rem;
+  margin-bottom: 1.5rem;
+}
+
+::v-deep .article-content h6 {
+  font-size: 1.125rem;
+  font-weight: 700;
+  line-height: 1.75rem;
+  margin-bottom: 1.5rem;
+}
+
+::v-deep .article-content p {
+  margin-bottom: 1.5rem;
+}
+
+::v-deep .article-content ul,
+::v-deep .article-content ol {
+  margin-bottom: 1.5rem;
+}
+
+::v-deep .article-content ul {
+  list-style: disc;
+  padding-left: 1.125rem;
+}
+
+::v-deep .article-content ol {
+  list-style: decimal;
+  padding-left: 1.5rem;
+}
+
+::v-deep .article-content blockquote {
+  border-left: 5px solid #cecece;
+  padding-left: 1.25rem;
+  margin-bottom: 1.5rem;
+}
+
+::v-deep .article-content pre {
+  margin-bottom: 1.5rem;
+}
+
+::v-deep .article-content pre code {
+  background: transparent;
+  padding: 0;
+}
+</style>
