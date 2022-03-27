50ec303da73264758fc19651665d549f0d2ca0f3
Melinda Golden <info@melindagolden.com>
2022-03-27 12:49:43 -0400

    add: install vue-gtag for google analytics

--- src/main.js
+++ src/main.js
@@ -2,8 +2,20 @@
 require("~/main.css");
 
 import DefaultLayout from "~/layouts/Default.vue";
+import VueGtag from "vue-gtag";
 
-export default function (Vue) {
+export default function (Vue, { router, isClient }) {
   // Set default layout as a global component
   Vue.component("Layout", DefaultLayout);
+
+  // Configure Google analytics
+  if (isClient) {
+    Vue.use(
+      VueGtag,
+      {
+        config: { id: process.env.GRIDSOME_GOOGLE_TAG }
+      },
+      router
+    );
+  }
 }
