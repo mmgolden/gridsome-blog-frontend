d1cfd817203db98c9b40b990239e4e389fe87e1f
Melinda Golden <info@melindagolden.com>
2022-04-12 20:02:56 -0400

    add: update copy in about page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -13,30 +13,27 @@
         alt="Melinda Golden standing on rock"
       />
 
-      <p class="text-lg font-medium text-navy dark:text-gray pt-10 pb-6">
-        Hi, my name is Melinda Golden. I'm a front-end developer based in
-        Asheville, NC. During my 3+ years in software development, I have used
-        Vue, React, TypeScript, Apollo, and GraphQL.
+      <p class="text-lg font-medium text-navy dark:text-gray pt-10 pb-7">
+        Hi, my name is Melinda Golden. I’m a front-end developer based in
+        Asheville, NC. I have experience with Vue, React, TypeScript, Apollo,
+        and GraphQL. What I enjoy about development is bringing together design
+        with code. There isn’t a better feeling than creating something from
+        nothing. I get excited about coding a beautiful UI, and I can honestly
+        say that I love CSS. That’s why I specialize in front-end development.
       </p>
-      <p class="text-lg font-medium text-navy dark:text-gray pb-6">
-        What I enjoy about development is bringing together design with code.
-        For me, there isn't a better feeling than creating something from
-        nothing. I get excited about coding a beautiful UI and I can honestly
-        say that I love CSS. That's why I specialize in front-end development.
-      </p>
-      <p class="text-lg font-medium text-navy dark:text-gray pb-6">
+      <p class="text-lg font-medium text-navy dark:text-gray pb-7">
         Before I got into software development, I owned my own wedding
         photography business. I began teaching myself how to build websites with
         WordPress. Then, I started working at a web design company as a web
         developer and stayed there for three years. While working full-time, I
         completed an online bootcamp in full-stack JavaScript.
       </p>
-      <p class="text-lg font-medium text-navy dark:text-gray pb-6">
-        While I spend much of my free time learning, I also regularly attend
-        local tech meetups. I am the organizer for the Asheville Women in Tech
-        meetup. And I have volunteered to give presentations about JavaScript
-        and share my knowledge with other developers. Besides coding, I enjoy
-        traveling, hiking, kayaking, and photography.
+      <p class="text-lg font-medium text-navy dark:text-gray pb-7">
+        While I spend much of my free time learning, I regularly attend local
+        tech meetups. I am the organizer for the Asheville Women in Tech meetup.
+        I have volunteered to give presentations about JavaScript and share my
+        knowledge with other developers. Besides coding, I enjoy traveling,
+        hiking, kayaking, and photography.
       </p>
       <p class="text-lg font-medium text-navy dark:text-gray pb-6">
         You can find me on Twitter
