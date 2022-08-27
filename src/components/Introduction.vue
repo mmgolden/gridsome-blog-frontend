debcd5caa985ff2bd52707b06d40cf64f039b0eb
Melinda Golden <info@melindagolden.com>
2022-08-27 13:19:08 -0400

    refactor: change introduction to single column

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -1,27 +1,25 @@
 <template>
-  <div class="container mx-auto lg:grid grid-cols-3 gap-4 py-44 px-4 2xl:px-0">
-    <h2 class="text-4xl font-bold pb-5 lg:pb-0 text-navy dark:text-gray">
-      I bridge the gap between design and code.
-    </h2>
-
-    <p class="text-lg font-medium pb-5 lg:pb-0 text-navy dark:text-gray">
-      I have experience with Vue, React, TypeScript, Apollo, and GraphQL. What I
-      enjoy about development is bringing together design with code.
-    </p>
-
-    <div class="flex flex-col">
-      <p class="text-lg font-medium pb-5 lg:pb-0 text-navy dark:text-gray">
-        There isn’t a better feeling than creating something from nothing. I get
-        excited about coding a beautiful UI.
-      </p>
+  <div class="container mx-auto py-44 px-4 2xl:px-0">
+    <div class="flex flex-col lg:flex-row lg:justify-between pb-16">
+      <h2 class="text-4xl font-bold pb-5 lg:pb-0 text-navy dark:text-gray">
+        About
+      </h2>
       <g-link
-        class="flex items-center mr-auto lg:mr-0 lg:ml-auto text-lg font-bold text-navy dark:text-gray"
+        class="link flex items-center mr-auto lg:mr-0 lg:ml-auto text-lg font-bold text-navy dark:text-gray"
         to="/about/"
       >
         Read more
         <ArrowRight class="ml-1 stroke-current text-navy dark:text-gray" />
       </g-link>
     </div>
+
+    <p class="text-lg font-medium pb-5 lg:pb-0 text-navy dark:text-gray">
+      I have experience with Vue, React, TypeScript, Apollo, and GraphQL. What I
+      enjoy about development is bringing together design with code. There isn’t
+      a better feeling than creating something from nothing. I get excited about
+      coding a beautiful UI, and I can honestly say that I love CSS. That’s why
+      I specialize in front-end development.
+    </p>
   </div>
 </template>
 
@@ -34,3 +32,9 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.link {
+  height: 29px;
+}
+</style>
