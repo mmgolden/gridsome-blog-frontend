f064276332059688ac78c75a52ed4285505213c0
Melinda Golden <info@melindagolden.com>
2022-11-15 08:58:26 -0500

    chore: remove Google analytics and install Plausible

--- src/main.js
+++ src/main.js
@@ -1,21 +1,10 @@
 // Import global styles
 import "~/main.css";
 
+// Import layout
 import DefaultLayout from "~/layouts/Default.vue";
-import VueGtag from "vue-gtag";
 
-export default function (Vue, { router, isClient }) {
+export default function (Vue) {
   // Set default layout as a global component
   Vue.component("Layout", DefaultLayout);
-
-  // Configure Google analytics
-  if (isClient) {
-    Vue.use(
-      VueGtag,
-      {
-        config: { id: process.env.GRIDSOME_GOOGLE_TAG }
-      },
-      router
-    );
-  }
 }
